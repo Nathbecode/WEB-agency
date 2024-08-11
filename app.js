aa3439b3745810180307d2466ed16a389003c8a6
Nathaly V <166789478+Nathbecode@users.noreply.github.com>
2024-08-11 13:52:00 +0200

    order

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 // Get modal element
 var modal = document.getElementById('myModal');
 
-// Get open modal button
+// Get open modal buttons
 var openModalBtn = document.getElementById('openModalBtn');
+var openModalBtn2 = document.getElementById('openModal2');
 
 // Get close button element
 var closeBtn = document.getElementsByClassName('close')[0];
 
+//Get email2
+var modalEmailInput = document.getElementById('email');
+var newsletterEmailInput = document.getElementById('email2');
+
 // Listen for open click
-openModalBtn.addEventListener('click', openModal);
+openModalBtn.addEventListener('click', function() {
+    openModal();
+});
+openModalBtn2.addEventListener('click', function() {
+    openModal(newsletterEmailInput.value);
+});
+
 
 // Listen for close click
 closeBtn.addEventListener('click', closeModal);
@@ -17,7 +28,10 @@ closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick);
 
 // Function to open modal
-function openModal() {
+function openModal(emailValue = '') {
+    if (emailValue) {
+        modalEmailInput.value = emailValue;
+    }
     modal.style.display = 'block';
 }
 
@@ -33,44 +47,37 @@ function outsideClick(e) {
     }
 }
 
-AOS.init();
-
-document.getElementById('email').addEventListener("input",checkEmail);
-function checkEmail (event) {
+// Email validation function
+document.getElementById('email').addEventListener("input", checkEmail);
+function checkEmail(event) {
     let email = event.target.value;
     let ermsg = document.getElementById('msgem');
-    if(!email.includes('@') || !email.includes('.')) {
+    if (!email.includes('@') || !email.includes('.')) {
         ermsg.style.display = 'unset';
         document.getElementById('email').style.backgroundColor = 'rgb(219,111,111)';
-    } else if (email.includes('@')&&email.includes('.')) {
+    } else if (email.includes('@') && email.includes('.')) {
         ermsg.style.display = 'none';
-        document.getElementById('email').style.backgroundColor='rgb(111,219,190)';
+        document.getElementById('email').style.backgroundColor = 'rgb(111,219,190)';
     }
 }
 
-document.getElementById('username').addEventListener("input",checkUser);
-function checkUser () {
+// Username validation function
+document.getElementById('username').addEventListener("input", checkUser);
+function checkUser() {
     let user = document.getElementById('username').value;
     let array = user.split('');
     let ermsg = document.getElementById('msgus');
-    if (array.length<5) {
+    if (array.length < 5 || array.length > 10 || !(/^[a-z]+$/.test(user))) {
         ermsg.style.display = 'unset';
         document.getElementById('username').style.backgroundColor = 'rgb(219,111,111)';
-    }  else if (array.length>10) {
-        ermsg.style.display = 'unset';
-        document.getElementById('username').style.backgroundColor = 'rgb(219,111,111)';
-    }  else if(!(/^[a-z]+$/).test(user)) {
-        ermsg.style.display = 'unset';
-        document.getElementById('username').style.backgroundColor = 'rgb(219,111,111)';
-        } 
-    else {
+    } else {
         ermsg.style.display = 'none';
-        document.getElementById('username').style.backgroundColor='rgb(111,219,190)';
+        document.getElementById('username').style.backgroundColor = 'rgb(111,219,190)';
     }
 }
 
-document.querySelector("form").addEventListener("submit", function (event) {
+// Prevent form submission
+document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log(`Your username is: ${document.getElementById('username').value},and your email is: ${document.getElementById('email').value}`); 
+    console.log(`Your username is: ${document.getElementById('username').value}, and your email is: ${document.getElementById('email').value}`);
 });
-
